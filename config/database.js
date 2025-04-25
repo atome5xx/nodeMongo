f2bc32d6176a28c2a9f931ea317d49b0980ee9a1
thomas letoublon <tletoubgym74@gmail.com>
2025-04-25 10:43:15 +0200

    config database et model emprunt

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/Movies');
+mongoose.connect('mongodb://localhost:27017/Labo');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
--- model/empruntModel.js
+++ model/empruntModel.js
@@ -5,11 +5,15 @@ const Schema = mongoose.Schema;
 const EmpruntSchema = new Schema({
     id: { type: Number, required: false },
     idUser: { type: Number, required: true },
-    id: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
-    emprunt: [Number],
+    idMateriel: { type: Number, required: true },
+    debutEmprunt: { type: Date, required: true },
+    finEmprunt: { type: Date, required: true },
+    isValid: {
+        type: String,
+        enum: ['En attente', 'Non Validé', 'Validé'],
+        default: 'En attente'
+    },
+    isRendu: { type: Boolean, default: false },
 }, { collection: 'Emprunts' });
 
 const EMPRUNT = mongoose.model('Emprunts', EmpruntSchema);
