810a45ad6a1747086219ce5c5d7c9cc1fbbf9b90
thomas letoublon <tletoubgym74@gmail.com>
2025-04-25 12:18:39 +0200

    ajout de userRoute

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config(); // Doit être appelé avant tout autre code
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const PORT = 3000;
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/connection', authRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
