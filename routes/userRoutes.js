305f744a6bcc255e9a46081f62aa34fb2f3bf774
thomas letoublon <tletoubgym74@gmail.com>
2025-04-25 12:24:13 +0200

    userroute complet

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,4 +2,16 @@ import express from "express";
 const router = express.Router();
 import userController from "../controller/userController.js";
 
+router.get('/:id', security, securityId, userController.getProfile);
 
+router.put('/', security, securityId, userController.updateUser);
+
+router.delete("/:id", security, securityId, userController.deleteUser);
+
+router.delete("/Favoris", security, securityId, userController.delEmprunt);
+
+router.post('/Favoris', security, securityId, userController.addEmprunt);
+
+router.get('/Favoris/:id', security, securityId, userController.affEmprunt);
+
+export default router;
