62fcd0dd9cc7ac5b4c6749837b659ce798ecc6f3
Rakan24 <113358163+Rakan24@users.noreply.github.com>
2025-07-08 18:16:14 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,14 @@ provider "azurerm" {
   features {}
 }
 
-variable "resource_group_name" {
-  type    = string
-  default = "SDFORM"
-}
-
 variable "location" {
   type    = string
   default = "francecentral"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "SDFORM"
 }
 
 variable "vm_name" {
@@ -17,32 +17,103 @@ variable "vm_name" {
   default = "VM-ADDS"
 }
 
-variable "os_disk_id" {
-  type = string
-  default = "/subscriptions/2f982fe1-7206-421f-abcd-b7575cbd9185/resourceGroups/SDFORM/providers/Microsoft.Compute/disks/VM-ADDS_OsDisk_1_91f0471096c14d24b07aa326af76f4ff"
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-variable "network_interface_id" {
-  type = string
-  default = "/subscriptions/2f982fe1-7206-421f-abcd-b7575cbd9185/resourceGroups/SDFORM/providers/Microsoft.Network/networkInterfaces/vm-adds676"
-}
-
-resource "azurerm_windows_virtual_machine" "vm_adds" {
-  name                = var.vm_name
-  resource_group_name = var.resource_group_name
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vm_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.vm_name}-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "AllowRDP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  description                 = "Autoriser l'accès RDP"
+}
+
+resource "azurerm_network_security_rule" "nodejs" {
+  name                        = "AllowNode3000"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "3000"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  description                 = "Autoriser l'accès Node.js port 3000"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
   size                = "Standard_B2s"
   admin_username      = "admin_sd"
-  admin_password      = "ChangeMe123!"  # ⚠️ À stocker dans un fichier .tfvars ou Vault en prod
+  admin_password      = "ChangeMe123!" # ⚠️ Ne jamais versionner ça en production
 
   network_interface_ids = [
-    var.network_interface_id
+    azurerm_network_interface.nic.id
   ]
 
   os_disk {
+    name                 = "${var.vm_name}-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "StandardSSD_LRS"
-    managed_disk_id      = var.os_disk_id
     disk_size_gb         = 127
   }
 
@@ -53,6 +124,12 @@ resource "azurerm_windows_virtual_machine" "vm_adds" {
     version   = "latest"
   }
 
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = "admin_sd"
+    admin_password = "ChangeMe123!"
+  }
+
   os_profile_windows_config {
     provision_vm_agent             = true
     enable_automatic_upgrades     = true
@@ -61,52 +138,9 @@ resource "azurerm_windows_virtual_machine" "vm_adds" {
     require_guest_provision_signal = true
   }
 
-  security_type = "TrustedLaunch"
-
   boot_diagnostics {
     enabled = true
   }
-}
 
-# === NSG for allowing Node.js on port 3000 ===
-
-resource "azurerm_network_security_group" "main_nsg" {
-  name                = "vm-adds-nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_network_security_rule" "allow_node_3000" {
-  name                        = "AllowAnyCustom3000Inbound"
-  priority                    = 110
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "3000"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.main_nsg.name
-  description                 = "Autorise l'accès HTTP pour Node.js sur le port 3000"
-}
-
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = var.network_interface_id
-  network_security_group_id = azurerm_network_security_group.main_nsg.id
-}
-
-resource "azurerm_network_security_rule" "allow_rdp_3389" {
-  name                        = "AllowAnyRDPInbound"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "3389"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.main_nsg.name
-  description                 = "Autorise l'accès RDP pour l'administration de la VM"
+  security_type = "TrustedLaunch"
 }
