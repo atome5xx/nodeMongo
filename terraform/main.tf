fa531f5b414a739bb777938c1735982329bde4e9
Rakan24 <113358163+Rakan24@users.noreply.github.com>
2025-07-08 18:12:12 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,3 +95,18 @@ resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = var.network_interface_id
   network_security_group_id = azurerm_network_security_group.main_nsg.id
 }
+
+resource "azurerm_network_security_rule" "allow_rdp_3389" {
+  name                        = "AllowAnyRDPInbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.main_nsg.name
+  description                 = "Autorise l'accès RDP pour l'administration de la VM"
+}
